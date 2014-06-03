1119c4f508f31214a25db0ee534a303380cb592b
tjmullen2 <tjmullen2@gmail.com>
2014-06-03 14:03:48 -0700

    Cleaned up and documented more clearly

--- TakeHomePhotoViewer/MainPage.xaml.cs
+++ TakeHomePhotoViewer/MainPage.xaml.cs
@@ -13,29 +13,29 @@ namespace TakeHomePhotoViewer
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        /// <summary>
-        /// View model for the page
-        /// </summary>
         private PhotoCollectionViewModel _viewModel;
+
+        /// <summary>
+        /// View model for the page to use MVVM design pattern (uses lazy loading to conserve startup time)
+        /// </summary>
         public PhotoCollectionViewModel ViewModel { get { return _viewModel ?? (_viewModel = new PhotoCollectionViewModel()); } set { _viewModel = value; } }
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
             // Only using CameraRoll repository for now
             PhotoCollection.RegisterRepository(new CameraRollRepository());
 
-            var availableSourceIds = await PhotoCollection.GetAvailableImageRepositoriesAsync();
+            var availableSourceIds = PhotoCollection.GetAvailableImageRepositoriesAsync();
+
+            // For now, we are only concerned with the first repository (CameraRoll)
             ViewModel = new PhotoCollectionViewModel(availableSourceIds[0]);
 
             DataContext = ViewModel;
@@ -48,31 +48,26 @@ namespace TakeHomePhotoViewer
             base.OnNavigatedFrom(e);
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
         private void CollectionImage_OnTap(object sender, GestureEventArgs e)
         {
+            if (sender == null || sender as FrameworkElement == null)
+                return;                                                 // Fall out gracefully to prevent invalid casts
+
             var imageDetails = ((FrameworkElement) sender).DataContext as ImageSnapshotInfo;
             if (imageDetails == null)
-                return; // We encountered something that could not be obtained from the datacontext, so prevent error
+                return;                                                 // We encountered something that could not be obtained from the datacontext, so prevent error
 
-            string urlString = string.Format("/PhotoDetailPage.xaml?ImageId={0}&SourceId={1}", HttpUtility.UrlEncode(imageDetails.Id), HttpUtility.UrlEncode(imageDetails.SourceId));
+            string urlString = string.Format("/PhotoDetailPage.xaml?ImageId={0}&SourceId={1}",  HttpUtility.UrlEncode(imageDetails.Id), 
+                                                                                                HttpUtility.UrlEncode(imageDetails.SourceId));
             NavigationService.Navigate(new Uri(urlString, UriKind.Relative));
         }
 
+        /// <summary>
+        /// Used to trigger a data request from the repository when RadDataBoundListBox needs more information
+        /// Needed for data virtualization and to keep memory footprint low with many images
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void PhotoCollectionListBox_OnDataRequested(object sender, EventArgs e)
         {
             var results = await PhotoCollection.GetImageCollection(ViewModel.SourceId, ViewModel.ImageCollection.Count, 20);
