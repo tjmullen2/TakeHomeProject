bdda91286521e6d01ee38cd60886427cdfd761dc
tjmullen2 <tjmullen2@gmail.com>
2014-06-08 05:23:25 -0700

    Added support for double-tap on detail image to show source (suggested in a web browser) if the repository source is a web source

--- TakeHomePhotoViewer/PhotoDetailPage.xaml.cs
+++ TakeHomePhotoViewer/PhotoDetailPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Windows.Input;
 using System.Windows.Navigation;
+using Microsoft.Phone.Tasks;
 using TakeHomePhotoViewer.PhotoSDK;
 using TakeHomePhotoViewer.ViewModels;
 
@@ -11,6 +14,14 @@ namespace TakeHomePhotoViewer
         {
             InitializeComponent();
             DataContext = App.ViewModel;
+        }
+
+        private void Image_OnDoubleTap(object sender, GestureEventArgs e)
+        {
+            // If we want to expand this by implementing double-tap on the image, 
+            // we could open a browser to the link (would require testing for link available and valid)
+
+            // var webBrowserTask = new WebBrowserTask{ Uri = new Uri(App.ViewModel.SelectedImage.ImageMetadata["link"])}
         }
     }
 }
